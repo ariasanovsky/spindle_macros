6e38b3ec67eb65157904d2ec7c596d9ed63dc8d1
Alex Riasanovsky <a.riasanovsky@gmail.com>
2023-07-15 14:12:15 -0400

    colored output on terminal & warning

--- src/error.rs
+++ src/error.rs
@@ -1,26 +1,28 @@
-use std::fmt::Display;
+// use std::fmt::Display;
 
-use proc_macro2::{TokenStream, Ident};
-use syn::Generics;
+use proc_macro2::TokenStream;
 
-use crate::TokenResult;
+// use proc_macro2::{TokenStream, Ident};
+// use syn::Generics;
 
-pub(super) enum Error<'a> {
-    _UnexpectedAttribute(&'a Ident),
-    _UnexpectedGenerics(&'a Generics),
-}
+// use crate::TokenResult;
 
-impl From<Error<'_>> for TokenStream {
-    fn from(value: Error<'_>) -> Self {
-        todo!()
-    }
-}
+// pub(super) enum Error<'a> {
+//     _UnexpectedAttribute(&'a Ident),
+//     _UnexpectedGenerics(&'a Generics),
+// }
 
-impl Display for Error<'_> {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        todo!()
-    }
-}
+// impl From<Error<'_>> for TokenStream {
+//     fn from(value: Error<'_>) -> Self {
+//         todo!()
+//     }
+// }
+
+// impl Display for Error<'_> {
+//     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+//         todo!()
+//     }
+// }
 
 pub(super) trait NaivelyTokenize: Sized + ToString {
     fn naively_tokenize(self) -> TokenStream {
@@ -31,3 +33,32 @@ pub(super) trait NaivelyTokenize: Sized + ToString {
 
 impl NaivelyTokenize for std::io::Error {}
 impl NaivelyTokenize for serde_json::Error {}
+impl NaivelyTokenize for String {
+    fn naively_tokenize(self) -> TokenStream {
+        println!("{self}");
+        return quote::quote! { "compiler error: see terminal" };    
+        // todo!("sanitize #self")
+    }
+}
+
+pub(super) fn command_output_result(output: std::process::Output) -> Result<String, String> {
+    let msg = match (&output.stderr, &output.stdout) {
+        (err, out) if !err.is_empty() && !out.is_empty() => {
+            format!("{}\n{}", String::from_utf8_lossy(&err), String::from_utf8_lossy(&out))
+        },
+        (err, _) if !err.is_empty() => {
+            String::from_utf8_lossy(&err).to_string()
+        },
+        (_, out) if !out.is_empty() => {
+            String::from_utf8_lossy(&out).to_string()
+        },
+        _ => {
+            String::from_utf8_lossy(&output.status.to_string().as_bytes()).to_string()
+        },
+    };
+    if output.status.success() {
+        Ok(msg)
+    } else {
+        Err(msg)
+    }
+}
--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ use std::path::PathBuf;
 use proc_macro2::TokenStream;
 use quote::ToTokens;
 use serde::{Deserialize, Serialize};
-use syn::{parse_macro_input, token::Token};
+use syn::{parse_macro_input, /* token::Token */};
 
-use crate::error::{NaivelyTokenize};
+use crate::error::{NaivelyTokenize, command_output_result};
 
 mod error;
 mod parse;
@@ -33,65 +33,128 @@ struct RangeAttributes;
 #[derive(Clone)]
 struct RangeFn(syn::ItemFn);
 
+impl ToTokens for RangeFn {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        self.0.to_tokens(tokens);
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
 struct RangeSpindle {
     home: String,
     name: String,
-    device: String,
+    populated: bool,
     compiled: bool,
+    device: Option<String>,
+    msg: Option<String>,
+    kernel: Option<String>,
 }
+
+static RANGE_FILES: &[(&str, &str, &str)] = &[
+    ("Cargo.toml", "", include_str!("range/Cargo.toml")),
+    ("rust-toolchain.toml", "", include_str!("range/rust-toolchain.toml")),
+    ("config.toml", ".cargo", include_str!("range/.cargo/config.toml")),
+    ("device.rs", "src", ""),
+    ("lib.rs", "src", include_str!("range/src/lib.rs")),
+];
 
 impl RangeSpindle {
     fn generate(name: &str, device: RangeFn) -> Result<Self, TokenStream> {
-        let crate_json = PathBuf::from(KERNELS).join(name).with_extension("json");
-        if crate_json.exists() {
-            let json = std::fs::read_to_string(crate_json).map_err(NaivelyTokenize::naively_tokenize)?;
-            let spindle: RangeSpindle = serde_json::from_str(&json).map_err(NaivelyTokenize::naively_tokenize)?;
-            return Ok(spindle);
-        }
+        let spindle = PathBuf::from(KERNELS).join(name).with_extension("json");
+        let new_device = device.clone().into_token_stream().to_string();
+        let spindle = if spindle.exists() {
+            let spindle = std::fs::read_to_string(spindle).map_err(NaivelyTokenize::naively_tokenize)?;
+            let mut spindle: RangeSpindle = serde_json::from_str(&spindle).map_err(NaivelyTokenize::naively_tokenize)?;
+            spindle.update_device(new_device)?;
+            spindle
+        } else {
+            Self {
+                home: String::from(KERNELS),
+                name: name.into(),
+                populated: false,
+                compiled: false,
+                device: Some(new_device),
+                msg: None,
+                kernel: None,
+            }
+        };
         let path = PathBuf::from(KERNELS).join(name);
         std::fs::create_dir_all(&path).map_err(NaivelyTokenize::naively_tokenize)?;
-        std::fs::create_dir(path.join(".cargo")).map_err(NaivelyTokenize::naively_tokenize)?;
-        std::fs::create_dir(path.join("src")).map_err(NaivelyTokenize::naively_tokenize)?;
-        std::fs::write(
-            path.join("Cargo.toml"),
-            include_str!("range/Cargo.toml")
-        ).map_err(NaivelyTokenize::naively_tokenize)?;
-        std::fs::write(
-            path.join("rust-toolchain.toml"),
-            include_str!("range/rust-toolchain.toml")
-        ).map_err(NaivelyTokenize::naively_tokenize)?;
-        std::fs::write(
-            path.join(".cargo/config.toml"),
-            include_str!("range/.cargo/config.toml")
-        ).map_err(NaivelyTokenize::naively_tokenize)?;
-        std::fs::write(
-            path.join("src/lib.rs"),
-            include_str!("range/src/lib.rs")
-        ).map_err(NaivelyTokenize::naively_tokenize)?;
-        let device = device.0.into_token_stream().to_string();
-        std::fs::write(
-            path.join("src/device.rs"),
-            &device
-        ).map_err(NaivelyTokenize::naively_tokenize)?;
+        std::fs::create_dir_all(path.join(".cargo")).map_err(NaivelyTokenize::naively_tokenize)?;
+        std::fs::create_dir_all(path.join("src")).map_err(NaivelyTokenize::naively_tokenize)?;
+        if !spindle.populated {
+            for (name, dir, contents) in RANGE_FILES {
+                if contents.ne(&"") {
+                    std::fs::write(path.join(dir).join(name), contents)
+                        .map_err(NaivelyTokenize::naively_tokenize)?;
+                }
+            }
+            let device = device.into_token_stream().to_string();
+            std::fs::write(path.join("src/device.rs"), &device)
+                .map_err(NaivelyTokenize::naively_tokenize)?;    
+        }
         
-        let spindle = RangeSpindle {
-            home: KERNELS.into(),
-            name: name.into(),
-            device,
-            compiled: false,
-        };
-        spindle.serialize()?;
         Ok(spindle)
     }
 
-    fn serialize(&self) -> Result<(), TokenStream> {
+    fn remove_files(&mut self) -> Result<(), TokenStream> {
+        let path = PathBuf::from(&self.home).join(&self.name);
+        for (file, dir, _) in RANGE_FILES {
+            let path = path.join(dir).join(file);
+            if path.exists() {
+                std::fs::remove_file(path).map_err(NaivelyTokenize::naively_tokenize)?;
+            }
+        }
+        let Self {
+            home: _home,
+            name: _name,
+            populated,
+            compiled,
+            device,
+            msg,
+            kernel
+        } = self;
+        
+        *populated = false;
+        *compiled = false;
+        *device = None;
+        *msg = None;
+        *kernel = None;
+        self.write()?;
+        Ok(())
+    }
+
+    fn update_device(&mut self, new_device: String) -> Result<(), TokenStream> {
+        let Self {
+            home: _,
+            name: _,
+            populated,
+            compiled,
+            device,
+            msg,
+            kernel
+        } = self;
+
+        if device.as_ref().is_some_and(|device| new_device.eq(device)) {
+            return Ok(())
+        }
+        
+        *compiled = false;
+        *populated = false;
+        *device = Some(new_device);
+        *msg = None;
+        *kernel = None;
+        self.remove_files()?;
+        self.write()
+    }
+
+    fn write(&self) -> Result<(), TokenStream> {
         let json = serde_json::to_string_pretty(&self).map_err(NaivelyTokenize::naively_tokenize)?;
         let crate_json = PathBuf::from(&self.home).join(&self.name).with_extension("json");
         std::fs::write(crate_json, json).map_err(NaivelyTokenize::naively_tokenize)
     }
 
-    fn compile(&mut self) -> Result<(), TokenStream> {
+    fn compile(&mut self) -> Result<String, TokenStream> {
         let mut cmd = std::process::Command::new("cargo");
         let home = format!("{}/{}", self.home, self.name);
         cmd.args([
@@ -103,17 +166,23 @@ impl RangeSpindle {
             "build",
             "--release",
         ]);
-        dbg!(&cmd);
         let output = cmd.output().map_err(NaivelyTokenize::naively_tokenize)?;
-        println!("{}", String::from_utf8_lossy(&output.stdout));
-        println!("{}", String::from_utf8_lossy(&output.stderr));
-        // .map_err(NaivelyTokenize::naively_tokenize)?;
-        // dbg!(&output);
-        todo!()
+        let output = match command_output_result(output) {
+            Ok(output) => output,
+            Err(err) => {
+                self.msg = Some(err.to_string());
+                self.compiled = false;
+                self.write()?;
+                return Err(err.naively_tokenize());
+            }
+        };
+        self.compiled = true;
+        self.write()?;
+        Ok(output)
     }
 }
 
-static KERNELS: &'static str = "target/kernels";
+static KERNELS: &'static str = "target/kernels/";
 // static RANGE_KERNEL: &'static str = include_str!("range/src/lib.rs");
 // static RANGE_CARGO_TOML: &'static str = include_str!("range/Cargo.toml");
 
@@ -131,12 +200,26 @@ impl RangeFn {
     }
 }
 
-fn emit_range_kernel(attr: RangeAttributes, item: RangeFn) -> TokenResult {
+fn emit_range_kernel(_attr: RangeAttributes, item: RangeFn) -> TokenResult {
     let name = item.name();
     let mut device = item.clone();
     device.make_visible();
     device.rename("device");
     let mut spindle = RangeSpindle::generate(&name, device)?;
-    spindle.compile()?;
-    Ok(item.0.into_token_stream())
+    // dbg!(&spindle);
+    const WARNING: &'static str = "\
+        #![no_std] \
+        #![feature(abi_ptx)] \
+        #![feature(stdsimd)] \
+        #![feature(core_intrinsics)] \
+        core::arch::nvptx::*; \
+    ";
+    const COLOR: &'static str = "\x1b[33m";
+    const RESET: &'static str = "\x1b[0m";
+    println!("{COLOR}{name} uses {}{}", WARNING, RESET);
+    let output = spindle.compile()?;
+    println!("{}", output.trim_end());
+    Ok(quote::quote! {
+        #item
+    })
 }
